4597e9ba801bcd120323b9e0588877ef655917d4
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:07:33 +0700

    tugas niomic
    tugas niomic

---
+++ tugas8.js
@@ -0,0 +1,16 @@
+function tugas8(){
+    let motor = {
+        merk: 'honda',
+        type: 'kopling',
+        harga: 15000000,
+        tahun: 2018,
+        warna: 'hitam'
+    }
+    console.log('Awal ');
+    console.log(motor);
+    motor.merk = 'yamaha';
+    console.log('Perubahan :');
+    console.log(motor);
+}
+
+tugas8();
